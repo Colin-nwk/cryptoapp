0e5c60295a30e3464813ae963d8805722b8c4c7a
colin-nwk <collinsnwoko05@gmail.com>
2022-03-22 04:46:31 +0100

    home stats

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,48 @@ import React from "react";
 import millify from "millify";
 import { Typography, Row, Col, Statistic } from "antd";
 import { Link } from "react-router-dom";
+import { useGetCryptosQuery } from "../services/cryptoApi";
 
 const { Title } = Typography;
 const Home = () => {
+  const { data, isFetching } = useGetCryptosQuery();
+  const globalState = data?.data?.stats;
+  console.log(globalState);
+  if (isFetching) return "Loading...";
   return (
     <>
       <Title level={2} className="heading">
         Global Crypto Stats
       </Title>
+      <Row>
+        <Col span={12}>
+          <Statistic title="Total Cryptocurrencies" value={globalState.total} />
+        </Col>
+        <Col span={12}>
+          <Statistic
+            title="Total Exchanges"
+            value={millify(globalState.totalExchanges)}
+          />
+        </Col>
+        <Col span={12}>
+          <Statistic
+            title="Total Market Cap"
+            value={millify(globalState.totalMarketCap)}
+          />
+        </Col>
+        <Col span={12}>
+          <Statistic
+            title="Total 24h Volume"
+            value={millify(globalState.total24hVolume)}
+          />
+        </Col>
+        <Col span={12}>
+          <Statistic
+            title="Total Markets"
+            value={millify(globalState.totalMarkets)}
+          />
+        </Col>
+      </Row>
     </>
   );
 };
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+//! keys remove
+const cryptoApiHeaders = {
+  "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+  "x-rapidapi-key": "",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest("/coins"),
+    }),
+  }),
+});
+console.log(cryptoApi);
+
+export const { useGetCryptosQuery } = cryptoApi;
